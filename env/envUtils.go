5517a78a3eeeeccc79cf7edea2aa2ef6e5ecb3bf
wuhen <15210204903@163.com>
2022-02-03 10:00:00 +0800

    增加环境变量获取方法中的回调处理

--- env/envUtils.go
+++ env/envUtils.go
@@ -11,11 +11,16 @@ type GetFn func() string
 type GetIntFn func() int
 
 // GetEnvOrDefault 获取环境变量或默认值
-func GetEnvOrDefault(name, defaultValue string) GetFn {
+func GetEnvOrDefault(name, defaultValue string, callback ...func(envStr string) string) GetFn {
 	envVal := os.Getenv(name)
 	if envVal == "" {
 		envVal = defaultValue
 	}
+
+	for _, c := range callback{
+		envVal = c(envVal)
+	}
+
 	logs.Debugf("获取环境变量[%s], 对应的值为: %s", name, envVal)
 	return func() string {
 		return envVal
@@ -23,8 +28,8 @@ func GetEnvOrDefault(name, defaultValue string) GetFn {
 }
 
 // GetEnvReplaceNewlineChar 获取环境变量值内的\n进行真实换行替换
-func GetEnvReplaceNewlineChar(name, defaultValue string) GetFn {
-	val := GetEnvOrDefault(name, defaultValue)()
+func GetEnvReplaceNewlineChar(name, defaultValue string, callback ...func(envStr string) string) GetFn {
+	val := GetEnvOrDefault(name, defaultValue, callback...)()
 	newVal := strings.ReplaceAll(val, "\\n", "\n")
 	return func() string {
 		return newVal
@@ -32,7 +37,7 @@ func GetEnvReplaceNewlineChar(name, defaultValue string) GetFn {
 }
 
 // GetIntEnvOrDefault 获取int类型的环境变量
-func GetIntEnvOrDefault(name string, defaultVal int) GetIntFn {
+func GetIntEnvOrDefault(name string, defaultVal int, callback ...func(envInt int) int) GetIntFn {
 	res := defaultVal
 	val := os.Getenv(name)
 	if val != "" {
@@ -40,6 +45,9 @@ func GetIntEnvOrDefault(name string, defaultVal int) GetIntFn {
 		if err == nil {
 			res = r
 		}
+	}
+	for _, c := range callback {
+		res = c(res)
 	}
 	return func() int {
 		return res
